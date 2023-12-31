bb2c94d905b9b9776666007f2804cd3c849dd2ff
realsulty <Sultanobm1@gmail.com>
2023-12-31 00:30:09 +0000

    input functions

--- main.js
+++ main.js
@@ -16,6 +16,32 @@ e.preventDefault()
     alert('Sumbited successfuly');
   }
   }
+
+  document.getElementById('cv').addEventListener('input', function() {
+    if (this.value.length === 3) {
+      this.blur(); // Removes focus from the input
+    }
+
+  });
+
+  document.getElementById('Exp-mm').addEventListener('input', function() {
+    if (this.value.length === 2) { // Assuming 10 is the max length
+      const nextInput = this.nextElementSibling;
+      if (nextInput && nextInput.tagName === "INPUT") {
+        nextInput.focus(); // Focus on the next input element
+      }
+    }
+  });
+  document.getElementById('Exp-yy').addEventListener('input', function() {
+    if (this.value.length === 2) { // Assuming 10 is the max length
+      const nextInput = this.nextElementSibling;
+      if (nextInput && nextInput.tagName === "INPUT") {
+        nextInput.focus(); // Focus on the next input element
+      }
+    }
+  });
+  
+  
   
 
 
